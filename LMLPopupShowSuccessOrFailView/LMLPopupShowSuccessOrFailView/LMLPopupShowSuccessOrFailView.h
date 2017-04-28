5ed5e161ab562e16f331fb63e2715a36b4a4960a
LiaoMalin <liaodalin19903@126.com>
2017-04-28 15:38:25 +0800

    first commit

---
+++ LMLPopupShowSuccessOrFailView/LMLPopupShowSuccessOrFailView/LMLPopupShowSuccessOrFailView.h
@@ -0,0 +1,13 @@
+//
+//  LMLPopupShowSuccessOrFailView.h
+//  LMLPopupShowSuccessOrFailView
+//
+//  Created by ldl on 2017/4/28.
+//  Copyright © 2017年 liaomalin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LMLPopupShowSuccessOrFailView : UIView
+
+@end
---
+++ LMLPopupShowSuccessOrFailView/LMLPopupShowSuccessOrFailView/LMLPopupShowSuccessOrFailView.m
@@ -0,0 +1,21 @@
+//
+//  LMLPopupShowSuccessOrFailView.m
+//  LMLPopupShowSuccessOrFailView
+//
+//  Created by ldl on 2017/4/28.
+//  Copyright © 2017年 liaomalin. All rights reserved.
+//
+
+#import "LMLPopupShowSuccessOrFailView.h"
+
+@implementation LMLPopupShowSuccessOrFailView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
