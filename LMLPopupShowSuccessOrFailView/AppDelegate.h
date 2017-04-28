006459fc2413533c638888f3009fb65d024eae6c
LiaoMalin <liaodalin19903@126.com>
2017-04-28 14:11:37 +0800

    Initial Commit

---
+++ LMLPopupShowSuccessOrFailView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMLPopupShowSuccessOrFailView
+//
+//  Created by ldl on 2017/4/28.
+//  Copyright © 2017年 liaomalin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
